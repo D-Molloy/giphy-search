4afa31a15d86b8a489f39094eaa1de90bbcfacdc
Denis Molloy <denis.molloy@gmail.com>
2017-07-27 12:02:01 -0400

    javascript finished

--- javascript/script.js
+++ javascript/script.js
@@ -7,8 +7,10 @@ var currentResponse;
 var gifClick=false;
 
 function displayGifs() {
+	// event.preventDefault();
     $('.gif-display').empty();   
 	var userSubmit = $(this).attr("data-name");
+	$('.search-title').text("Displaying GIFs of: "+ userSubmit);
 	console.log(userSubmit);
 	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userSubmit + "&limit=11&api_key=6181b0ebaf0342f1b3f5f8e23474b3fe"
 
@@ -16,23 +18,23 @@ function displayGifs() {
 		url: queryURL,
 		method: 'GET'
 	}).done(function(response) {
-	
-	//append the results to gif-diplay
-	// $('.gif-display').empty();
 		currentResponse = response;
 		console.log(currentResponse);
 		
+		/// change out 20 for i < response.length
 		for (var i = 0; i <= 10; i++) {
+			var gifDiv = $('<div class="item">');
+			var rating = response.data[i].rating;
+			var p = $("<p>").text("Rating: " + rating);
 			var gifImg = $('<img>');
 			gifImg.addClass('gif-img');
 			gifImg.attr('src', response.data[i].images.fixed_height_still.url);
-			$('.gif-display').append(gifImg);
-			console.log(gifImg);
+			gifDiv.append(p);
+			gifDiv.append(gifImg);
+			$('.gif-display').append(gifDiv);
+			// console.log(gifImg);
 			// gifImg.text(response.data[0].images.fixed_height_still.url);
 		}
-		// function switchUrl(){
-		// var clickedGif = $(this).attr("src", response.data[i].images.fixed_height.url);
-		// }
 		return currentResponse;
 	});
 }
@@ -42,10 +44,10 @@ function switchUrl(){
 	var gifIndex = $(this).index();
 
 	if (gifClick == false){
-		$(this).attr("src", currentResponse.data[gifIndex].images.fixed_height.url);
+		$(this).children('img').attr("src", currentResponse.data[gifIndex].images.fixed_height.url);
 		gifClick = true;
 	} else {
-		$(this).attr("src", currentResponse.data[gifIndex].images.fixed_height_still.url);
+		$(this).children('img').attr("src", currentResponse.data[gifIndex].images.fixed_height_still.url);
 		gifClick = false;
 	}
 	console.log(gifIndex);
@@ -54,6 +56,7 @@ function switchUrl(){
 
 function renderButtons(){
 	$('.btn-display').empty();
+	event.preventDefault();
 
 	for (var i = 0; i < politicians.length; i++) {
 		var polBtn = $('<button>');
@@ -64,23 +67,47 @@ function renderButtons(){
 		console.log(polBtn);
 	}
 
-	$('.submit-btn').on('click', function(event){
-		// event.preventDefault();
+//prepend the img tag with a p tag ("Rating: [object rating")
+// creat a div instead of an image, add a p and an image tag inside the div
 
-		var userPick = $('.entry-form').val().trim();
-		console.log(userPick);
-		politicians.push(userPick);
-		console.log(politicians);
-		$('.entry-form').empty();
-		// $('submit-btn').off();
-		renderButtons();
-	});
-
+//copy code from 13 - button triggered AJAx
 }
 
-$(document).on("click", ".politician", displayGifs);
-$(document).on("click", ".gif-img", switchUrl);
+$('.submit-btn').on('click', function(event){
+	event.preventDefault();
+	var userPick = $('.entry-form').val().trim();
+	if (userPick != ""){
+	
+	politicians.push(userPick);
+	
+	} else {
+		alert("Please enter a name!");
+	}
+	renderButtons();
+});
 
-renderButtons();
+$(document).on('keyup', function (e) {
+	event.preventDefault();
+	
+    if (e.keyCode == 13) {
+    	var userPick = $('.entry-form').val().trim();
+    	if (userPick != "") {
+        	politicians.push(userPick);
+        	renderButtons();
+        } else {
+        	alert("Please enter a name!");
+    	} 	
+	}
+	
+});
+
+
+$(window).load(function(){
+	renderButtons();
+})
+$(document).on("click", ".politician", displayGifs);
+$(document).on("click", ".item", switchUrl);
+
+
 
 
